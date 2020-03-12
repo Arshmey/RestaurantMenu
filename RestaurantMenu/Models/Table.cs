d5d60dc98c175b6ea358dc758a783ce90f0a8a27
Maksim V <maksim_789456@mail.ru>
2020-03-12 14:56:25 +0400

    Changes views in accordance with the terms of reference. Add some counts methods

--- RestaurantMenu/Models/Table.cs
+++ RestaurantMenu/Models/Table.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace RestaurantMenu.Models
@@ -6,9 +7,29 @@ namespace RestaurantMenu.Models
     [ViewComponent(Name = "Table")]
     public class Table : ViewComponent
     {
-        public IViewComponentResult Invoke(IEnumerable<Dish> dishes)
+        public IViewComponentResult Invoke(IEnumerable<Dish> dishes) => View(dishes);
+
+        public static string CountTime(Dish dish)
         {
-            return View(dishes);
+            int hours = TimeSpan.FromMinutes(dish.CookTime).Hours;
+            int ostMin = dish.CookTime - hours* 60;
+            switch (hours)
+            {
+                case 1:
+                    return $"{hours} час {ostMin} мин.";
+                case 2:
+                case 3:
+                case 4:
+                    return $"{hours} часа {ostMin} мин.";
+                default:
+                    return hours >= 5 ? $"{hours} часов {ostMin} мин." : $"{dish.CookTime} мин.";
+            }
+        }
+
+        public static string CountCalorie(Dish dish)
+        {
+            decimal finalCalorie = dish.Grams * (dish.Calorie / 100);
+            return $"{finalCalorie} ккал.";
         }
     }
 }
